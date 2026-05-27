77a4e0c85716cfdac5e7d2ea5374cb0094dbfc00
Brandon Kunitzer <b.m.kunitzer@gmail.com>
2026-05-27 16:30:38 -0700

    Add: longest-consecutive-sequence - submission-5

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-5.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_set<int> contains(nums.begin(), nums.end());
+        map<int, int> lens;
+        int best = 0;
+        for (const auto & num : nums) {
+            if (lens.contains(num)) {
+                continue;
+            }
+            int curr_len = 1;
+            int curr_num = num;
+            while (contains.contains(curr_num + 1)) {
+                if (lens.contains(curr_num + 1)) {
+                    curr_len += lens[curr_num + 1];
+                    break;
+                }
+                curr_num ++;
+                curr_len ++;
+            }
+            best = max(curr_len, best);
+            lens[num] = curr_len;
+        }
+        return best;
+    }
+};
